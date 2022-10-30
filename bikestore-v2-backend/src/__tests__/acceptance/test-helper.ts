deb1e666b2f58c1fa9e42690eb4f433ec49963e7
quihuang <quihuang2017@gmail.com>
2022-10-30 18:59:06 -0500

    Creación proyecto loopback

---
+++ bikestore-v2-backend/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {BikestoreV2BackendApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new BikestoreV2BackendApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: BikestoreV2BackendApplication;
+  client: Client;
+}
---
+++ bikestore-v2-backend/src/datasources/index.ts
@@ -0,0 +1,1 @@
+export * from './bike-store-mongo-db.datasource';
