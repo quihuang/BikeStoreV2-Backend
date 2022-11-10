614d95913461ec9517da69757a3dab6c27f6119c
Dairo Camacho <dairoleandro83@gmail.com>
2022-11-10 09:51:55 -0500

    fix: palabras en español y var de entorno

--- bikestore-v2-backend/src/services/authentication.service.ts
+++ bikestore-v2-backend/src/services/authentication.service.ts
@@ -1,50 +1,56 @@
-import { injectable, /* inject, */ BindingScope } from '@loopback/core';
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
 import passwordGenerator from 'password-generator';
 import CryptoJS from 'crypto-js';
 import jwt from 'jsonwebtoken';
-import { Workers } from '../models';
+import {Workers} from '../models';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AuthenticationService {
-  private secretkey: string;
-  
-  constructor(/* Add @inject to inject parameters */) {
-    this.secretkey = String(process.env.SECRET_KEY);
-  }
+  private secretKey = String(process.env.SECRET_KEY);
 
-  generarPassword() {
+  constructor(/* Add @inject to inject parameters */) {}
+
+  createPassword() {
     return passwordGenerator(12, false);
   }
 
-  encriptar(password: string) {
-    let encryptedData = CryptoJS.AES.encrypt(password, this.secretkey).toString();
+  encryptPassword(password: string) {
+    let encryptedData = CryptoJS.AES.encrypt(
+      password,
+      this.secretKey,
+    ).toString();
     return encryptedData;
   }
 
-  encriptarObjeto(data: any) {
-    let encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), this.secretkey).toString();
+  encryptObject(data: any) {
+    let encryptedData = CryptoJS.AES.encrypt(
+      JSON.stringify(data),
+      this.secretKey,
+    ).toString();
     return encryptedData;
   }
 
-  desencriptar(password: string) {
-    let bytes = CryptoJS.AES.decrypt(password, this.secretkey);
+  decryptPassword(password: string) {
+    let bytes = CryptoJS.AES.decrypt(password, this.secretKey);
     let decryptedData = bytes.toString(CryptoJS.enc.Utf8);
     return decryptedData;
   }
 
-  desencriptarObjeto(data: string) {
-    let bytes = CryptoJS.AES.decrypt(data, this.secretkey);
+  decryptObject(data: string) {
+    let bytes = CryptoJS.AES.decrypt(data, this.secretKey);
     let encryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
     return encryptedData;
   }
 
-
   generateTokenJWT(worker: Workers) {
-    let token = jwt.sign({
-      name: worker.name,
-      lastName: worker.lastName,
-      id: worker.id
-    }, this.secretkey, { expiresIn: 60 }
+    let token = jwt.sign(
+      {
+        name: worker.name,
+        lastName: worker.lastName,
+        id: worker.id,
+      },
+      this.secretKey,
+      {expiresIn: '1h'},
     );
     return token;
   }
@@ -53,23 +59,24 @@ export class AuthenticationService {
     let datos = {
       name: worker.name,
       lastName: worker.lastName,
-      id: worker.id
-    }
+      id: worker.id,
+    };
 
-    let minutes = 5
-    let token = jwt.sign({
-      data: this.encriptarObjeto(datos)
-    }, this.secretkey, { expiresIn: 60 * minutes }
+    let token = jwt.sign(
+      {
+        data: this.encryptObject(datos),
+      },
+      this.secretKey,
+      {expiresIn: '1h'},
     );
     return token;
   }
 
   validateTokenJWT(token: string) {
     try {
-      let valido = jwt.verify(token, this.secretkey)
-      return valido;
-    }
-    catch {
+      let valid = jwt.verify(token, this.secretKey);
+      return valid;
+    } catch {
       return false;
     }
   }
